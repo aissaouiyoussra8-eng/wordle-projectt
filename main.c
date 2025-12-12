6a61f97962c296233a5223691ec0512ae07c2cf4
aissaouiyoussra8-eng <aissaouiyoussra8@gmail.com>
2025-12-12 11:32:56 +0100

    Update main.c

--- main.c
+++ main.c
@@ -48,17 +48,6 @@ int main(int argc, char *argv[]) {
         if (argc > 2) n = atoi(argv[2]);
         printf("Running benchmark on %d words...\n", n);
         
-        // Silence output for benchmark? Or just print summary.
-        // For simplicity, I'll just run them and print average.
-        // I need to modify solve_game to return attempts or be silent.
-        // Since I can't easily change the signature without editing other files, 
-        // I'll just re-implement a simple loop here or accept the output.
-        // Actually, let's just run it and let it print.
-        
-        // To do it properly, I should refactor solve_game to return int.
-        // But for now, I'll just skip benchmark implementation details or do a simple one.
-        // Let's just do a simple loop calling solve_game.
-        
         for(int i=0; i<n; i++) {
             char *t = get_random_word(dict);
             solve_game(t, dict);
@@ -78,3 +67,4 @@ int main(int argc, char *argv[]) {
     free_dictionary(dict);
     return 0;
 }
+
